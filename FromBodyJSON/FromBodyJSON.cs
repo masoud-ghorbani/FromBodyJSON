5ca4f6f483bef54e61c3dc23a208f32c2a35a091
mr.masoudgh <mr.masoudgh@gmail.com>
2019-07-11 03:29:37 +0430

    Add project files.

---
+++ FromBodyJSON/FromBodyJSON.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace System.Web.Http
+{
+    /// <summary>
+    ///     An attribute that specifies that an action parameter comes only from the entity (JSON)
+    ///     body of the incoming System.Net.Http.HttpRequestMessage.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, Inherited = true, AllowMultiple = false)]
+    public sealed class FromBodyJSONAttribute : ParameterBindingAttribute
+    {
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            return new FromBodyJSONBinding(parameter);
+        }
+    }
+
+    public class FromBodyJSONBinding : HttpParameterBinding
+    {
+        public FromBodyJSONBinding(HttpParameterDescriptor parameter)
+        : base(parameter)
+        {
+
+        }
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = JObject.Parse(actionContext.Request.Content.ReadAsStringAsync().Result);
+                if (data.ContainsKey(this.Descriptor.ParameterName))
+                {
+                    Type objectType = Type.GetType(this.Descriptor.ParameterType.FullName);
+                    actionContext.ActionArguments[this.Descriptor.ParameterName] = data[this.Descriptor.ParameterName].ToObject(objectType);
+                }
+            }
+            catch
+            {
+            }
+
+            var taskSource = new TaskCompletionSource<object>();
+            taskSource.SetResult(null);
+            return taskSource.Task;
+        }
+    }
+}
